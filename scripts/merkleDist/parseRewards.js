9a545d5b5fe6077b8992ecd22fcb5f833463be79
Anyhowclick <howeilun@hotmail.com>
2021-03-12 17:41:51 +0800

    Use merkletreejs library instead of converted js scripts

--- scripts/merkleDist/parseRewards.js
+++ scripts/merkleDist/parseRewards.js
@@ -1,40 +1,29 @@
-const BalanceTree = require('./balanceTree').BalanceTree;
+const {MerkleTree} = require('merkletreejs');
 const ethers = require('ethers');
-const BigNumber = ethers.BigNumber;
+const BN = ethers.BigNumber;
+const solidityKeccak256 = ethers.utils.solidityKeccak256;
+const keccak256 = require('keccak256');
 
 module.exports.parseRewards = function (rewardInfo) {
   const cycle = rewardInfo.cycle;
   const userRewards = rewardInfo.userRewards;
-  const mappedInfo = Object.keys(userRewards).reduce((memo, account) => {
-    if (!ethers.utils.isAddress(account)) {
-      throw new Error(`Found invalid address: ${account}`);
-    }
-    const parsedAddress = ethers.utils.getAddress(account);
-    if (memo[parsedAddress]) throw new Error(`Duplicate address: ${parsed}`);
-    const parsedTokenAmounts = userRewards[account].cumulativeAmounts.map((amt) => BigNumber.from(amt));
-    memo[parsedAddress] = {
-      tokens: userRewards[account].tokens,
-      cumulativeAmounts: parsedTokenAmounts,
-    };
-    return memo;
-  }, {});
-
-  const treeElements = Object.keys(mappedInfo).map((account) => ({
-    account,
-    tokens: mappedInfo[account].tokens,
-    cumulativeAmounts: mappedInfo[account].cumulativeAmounts,
-  }));
-
-  const tree = new BalanceTree(cycle, treeElements);
+  // verify addresses (check duplicates, invalid) and convert cumulative amounts to BN
+  // with account as key
+  const mappedTokensAmounts = verifyAddressAndConvertCumulativeAmounts(userRewards);
+  // structure data to include account field
+  const treeElements = addAccountInMapping(mappedTokensAmounts);
+  // hash tree elements to leaves
+  const leaves = hashElements(treeElements, cycle);
+  const tree = new MerkleTree(leaves, keccak256, {sort: true});
   const userRewardsWithProof = treeElements.reduce((memo, {account}, index) => {
-    tokens = mappedInfo[account].tokens;
-    cumulativeAmounts = mappedInfo[account].cumulativeAmounts.map((amt) => amt.toHexString());
+    tokens = mappedTokensAmounts[account].tokens;
+    cumulativeAmounts = mappedTokensAmounts[account].cumulativeAmounts.map((amt) => amt.toHexString());
 
     memo[account] = {
       index,
       tokens,
       cumulativeAmounts,
-      proof: tree.getProof(cycle, index, account, tokens, cumulativeAmounts)
+      proof: tree.getHexProof(leaves[index]),
     };
     return memo;
   }, {});
@@ -45,3 +34,36 @@ module.exports.parseRewards = function (rewardInfo) {
     userRewards: userRewardsWithProof,
   };
 };
+
+function verifyAddressAndConvertCumulativeAmounts(userRewards) {
+  return Object.keys(userRewards).reduce((memo, account) => {
+    if (!ethers.utils.isAddress(account)) {
+      throw new Error(`Found invalid address: ${account}`);
+    }
+    const parsedAddress = ethers.utils.getAddress(account);
+    if (memo[parsedAddress]) throw new Error(`Duplicate address: ${parsed}`);
+    const parsedTokenAmounts = userRewards[account].cumulativeAmounts.map((amt) => BN.from(amt));
+    memo[parsedAddress] = {
+      tokens: userRewards[account].tokens,
+      cumulativeAmounts: parsedTokenAmounts,
+    };
+    return memo;
+  }, {});
+}
+
+function addAccountInMapping(mappedTokensAmounts) {
+  return Object.keys(mappedTokensAmounts).map((account) => ({
+    account,
+    tokens: mappedTokensAmounts[account].tokens,
+    cumulativeAmounts: mappedTokensAmounts[account].cumulativeAmounts,
+  }));
+}
+
+function hashElements(treeElements, cycle) {
+  return treeElements.map((element, index) =>
+    solidityKeccak256(
+      ['uint256', 'uint256', 'address', 'address[]', 'uint256[]'],
+      [cycle.toString(), index.toString(), element.account, element.tokens, element.cumulativeAmounts]
+    )
+  );
+}
