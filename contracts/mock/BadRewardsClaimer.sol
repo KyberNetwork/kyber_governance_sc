e9b139821c99643497692f98f297628098cb3e3c
Anyhowclick <howeilun@hotmail.com>
2021-03-15 16:09:35 +0800

    Shift files to rewardsDist folder, add interface and comments

--- contracts/mock/BadRewardsClaimer.sol
+++ contracts/mock/BadRewardsClaimer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-import '../governance/rewards/RewardsDistributor.sol';
+import '../rewardDistribution/RewardsDistributor.sol';
 
 contract BadRewardsClaimer {
   function claim(
